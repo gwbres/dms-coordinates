c8f3e1748f057b49b50d73414d8328da51df75b9
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2021-12-22 20:05:17 +0100

    make this lib self sufficient until map_3d is merge

--- src/lib.rs
+++ src/lib.rs
@@ -17,6 +17,23 @@ macro_rules! single_line_if_else {
     };
 }
 
+const EARTH_RADIUS: f64 = 6371E3_f64;
+
+/// Converts x (f64) to radians
+fn deg2rad (x: f64) -> f64 { x/180.0*std::f64::consts::PI }
+
+/// Returns distance (m) between two decimal degrees coordinates::
+/// coord1: (lat,lon), coord2: (lat, lon)
+fn distance (coord1: (f64,f64), coord2: (f64,f64)) -> f64 {
+    let dphi = deg2rad(coord2.0) - deg2rad(coord1.0);
+    let d_lambda = deg2rad(coord2.1) - deg2rad(coord1.1);
+    let a: f64 = (dphi / 2.0_f64).sin().powf(2.0_f64)
+        + deg2rad(coord1.0).cos() * deg2rad(coord2.0).cos()
+            * (d_lambda/2.0_f64).sin().powf(2.0_f64);
+    let c = 2.0_f64 * a.powf(0.5_f64).atan2((1.0-a).powf(0.5_f64));
+    EARTH_RADIUS * c
+}
+
 impl DMS {
     /// Builds new `D°M'S''` coordinates
     pub fn new (degrees: i32, minutes: i32, seconds: f64, bearing: char) -> Result<DMS, std::io::Error> {
@@ -103,7 +120,7 @@ impl DMS3d {
 
     /// Returns distance (m) between self and another DMS3d
     pub fn distance (&self, other: DMS3d) -> f64 {
-        map_3d::distance(
+        distance(
             (self.latitude.to_decimal_degrees(),self.longitude.to_decimal_degrees()),
             (other.latitude.to_decimal_degrees(),other.longitude.to_decimal_degrees())
         )
